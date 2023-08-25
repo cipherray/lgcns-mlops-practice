6d1a70478c22490445c79375cf793d86fb59b3cf
cipherray <109684249+cipherray@users.noreply.github.com>
2023-08-25 01:46:42 +0000

    Add logger to train.py

--- train.py
+++ train.py
@@ -25,18 +25,21 @@ from src.preprocess import preprocess_pipeline
 
 # 로그 들어갈 위치
 # 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
 
 sys.excepthook = handle_exception
 warnings.filterwarnings(action="ignore")
 
-
 if __name__ == "__main__":
+    logger.info("Loading data...")
+
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
 
     # X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+    logger.info("Preprocessing...")
     X = preprocess_pipeline.fit_transform(X=_X, y=y)
 
     # Data storage - 피처 데이터 저장
@@ -67,6 +70,7 @@ if __name__ == "__main__":
     # mlflow 의 결과로 나오는 artifact 저장 위치
     mlflow.set_tracking_uri("./mlruns")
 
+    logger.debug("Using mlflw to rack an eexperiment..")
     for i, params in enumerate(param_set):
         # mlflow ui 에서 Run Name 컬럼에 보여질 이름
         run_name = f"Run {i}"
@@ -135,6 +139,7 @@ if __name__ == "__main__":
     # best_run 에는 성능이 가장 좋은 모델에 대한 모든 정보가 있음
     best_run = mlflow.get_run(best_run_df.at[0, "run_id"])
     best_params = best_run.data.params
+    logger.info(f"Best hyper-parameter: {best_params}")
 
     # best_run 에서 model 정보만 가져옴
     best_model_uri = f"{best_run.info.artifact_uri}/model"
